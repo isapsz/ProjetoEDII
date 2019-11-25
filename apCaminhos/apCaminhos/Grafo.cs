4ed2f61d0b7cf07ea39da63ed72088549a427251
isapsz <42344186+isapsz@users.noreply.github.com>
2019-11-25 11:14:00 -0300

    aaa

--- apCaminhos/apCaminhos/Grafo.cs
+++ apCaminhos/apCaminhos/Grafo.cs
@@ -6,44 +6,41 @@ using System.Threading.Tasks;
 
 //ISABELA PAULINO DE SOUZA 18189, GUSTAVO FERRREIRA GITZEL 18194 e AMABILE PIETROBON FERREIRA 18198
 
-namespace apCaminhosMarte
+class Grafo
 {
-    class Grafo
+    /*
+     Matriz de inteiros que armazena a distância entre dois vértices do grafo.
+    */
+    protected int[,] adjacencia;
+
+    /*
+     Propriedade que retorna e altera a matriz de adjacência.
+    */
+    public int[,] Adjacencia
     {
-        /*
-         Matriz de inteiros que armazena a distância entre dois vértices do grafo.
-        */
-        protected int[,] adjacencia;
-
-        /*
-         Propriedade que retorna e altera a matriz de adjacência.
-        */
-        public int[,] Adjacencia
-        {
-            get => adjacencia;
-            set => adjacencia = value;
-        }
-
-        /*
-         Propriedade que retorna e altera uma posição da matriz de adjacência.
-        */
-        public int this[int i, int j]
-        {
-            get => adjacencia[i, j];
-            set
-            {
-                if (value > 0)
-                    adjacencia[i, j] = value;
-            }
-        }
-
-        /*
-         Construtor que recebe como parâmetro a quantidade máxima de linhas e colunas que a matriz de adjacência deverá ter.
-        */
-        public Grafo(int tamanhoLinhas, int tamanhoColunas)
-        {
-            adjacencia = new int[tamanhoLinhas, tamanhoColunas];
-        }
-        
+        get => adjacencia;
+        set => adjacencia = value;
     }
+
+    /*
+     Propriedade que retorna e altera uma posição da matriz de adjacência.
+    */
+    public int this[int i, int j]
+    {
+        get => adjacencia[i, j];
+        set
+        {
+            if (value > 0)
+                adjacencia[i, j] = value;
+        }
+    }
+
+    /*
+     Construtor que recebe como parâmetro a quantidade máxima de linhas e colunas que a matriz de adjacência deverá ter.
+    */
+    public Grafo(int tamanhoLinhas, int tamanhoColunas)
+    {
+        adjacencia = new int[tamanhoLinhas, tamanhoColunas];
+    }
+
 }
